8a20598f2689da94504a66980120df0b40e4af85
aholstrup1@microsoft.com <aholstrup1@microsoft.com@users.noreply.github.com>
2023-11-27 10:41:23 +0100

    Small fix

--- build/projects/System Application Modules/.AL-Go/PreCompileApp.ps1
+++ build/projects/System Application Modules/.AL-Go/PreCompileApp.ps1
@@ -5,13 +5,7 @@ Param(
 
 if($appType -eq 'app')
 {
-    #compilationParams is a hashtable. Print all keys and values
-    Write-Host "compilationParams:"
-    foreach ($key in $compilationParams.Keys) {
-        Write-Host "$key = $($compilationParams[$key])"
-    }
-
-    Compile-AppWithBcCompilerFolder $compilationParams | Out-Null
+    Write-Host "compilationParams: $compilationParams"
 
     $scriptPath = Join-Path $PSScriptRoot "../../../scripts/VerifyExecutePermissions.ps1" -Resolve
     . $scriptPath -ModulesDirectory $compilationParams.Value["appProjectFolder"]    
