5f3690e0f7248c5aa5133ccc381495bda6f719ac
aholstrup1 <aholstrup@microsoft.com>
2024-02-22 15:24:29 +0100

    Disable breaking changes check

--- build/scripts/PreCompileApp.ps1
+++ build/scripts/PreCompileApp.ps1
@@ -60,7 +60,7 @@ if($appType -eq 'app')
                 exit
             }
 
-            Enable-BreakingChangesCheck -AppSymbolsFolder $parameters.Value["appSymbolsFolder"] -AppProjectFolder $parameters.Value["appProjectFolder"] -BuildMode $appBuildMode | Out-Null
+            # Enable-BreakingChangesCheck -AppSymbolsFolder $parameters.Value["appSymbolsFolder"] -AppProjectFolder $parameters.Value["appProjectFolder"] -BuildMode $appBuildMode | Out-Null
         }
     }
 }
