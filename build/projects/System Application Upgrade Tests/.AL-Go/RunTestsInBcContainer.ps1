d395a46a7c95a52c510090977614e8825efb33fb
aholstrup1 <aholstrup@microsoft.com>
2024-03-25 13:31:14 +0100

    test

--- build/projects/System Application Upgrade Tests/.AL-Go/RunTestsInBcContainer.ps1
+++ build/projects/System Application Upgrade Tests/.AL-Go/RunTestsInBcContainer.ps1
@@ -2,5 +2,15 @@ Param(
     [Hashtable]$parameters
 )
 
+Invoke-ScriptInBcContainer -containerName "BC-20240325" -scriptblock { 
+    $appfile = "C:\Applications\BaseApp\test\Microsoft_Tests-TestLibraries.app"
+    if (Test-Path $appfile) {
+        Write-Host "Importing test libraries..."
+        Publish-NAVApp -Path $appfile -ServerInstance BC -SkipVerification
+    } else {
+        Write-Host "Test libraries not found."
+    }
+}
+
 $script = Join-Path $PSScriptRoot "../../../scripts/RunTestsInBcContainer.ps1" -Resolve
 . $script -parameters $parameters
