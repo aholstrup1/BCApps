05e7ed8bf1772fd7b46298736cb4e8cf64c7aca5
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2025-01-03 10:42:36 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -12,7 +12,7 @@
         .\build.ps1 -ALGoProject "Test Stability Tools" -AutoFill
 #>
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText', '', Justification = 'local build')]
-param
+param 
 (
     [Parameter(Mandatory=$true)]
     [string] $ALGoProject,
