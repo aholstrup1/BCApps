0898b92554da0ad82d2889dd8ab120dfb91ff07b
aholstrup1 <aholstrup1@users.noreply.github.com>
2025-01-16 10:17:44 +0000

    [main] Update AL-Go System Files from aholstrup1/AL-Go@private -  41c02eb60ad0eb2bf7da6e30b2db06315715122b / Related to AB#539394

--- build/projects/Business Foundation/.AL-Go/localDevEnv.ps1
+++ build/projects/Business Foundation/.AL-Go/localDevEnv.ps1
@@ -46,9 +46,9 @@ Write-Host -ForegroundColor Yellow @'
 
 $tmpFolder = Join-Path ([System.IO.Path]::GetTempPath()) "$([Guid]::NewGuid().ToString())"
 New-Item -Path $tmpFolder -ItemType Directory -Force | Out-Null
-$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/aholstrup1/AL-Go/private/aholstrup/555754/Actions/Github-Helper.psm1' -folder $tmpFolder
-$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/aholstrup1/AL-Go/private/aholstrup/555754/Actions/AL-Go-Helper.ps1' -folder $tmpFolder
-DownloadHelperFile -url 'https://raw.githubusercontent.com/aholstrup1/AL-Go/private/aholstrup/555754/Actions/Packages.json' -folder $tmpFolder | Out-Null
+$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/aholstrup1/AL-Go/private/aholstrup/555754_refactor/Actions/Github-Helper.psm1' -folder $tmpFolder
+$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/aholstrup1/AL-Go/private/aholstrup/555754_refactor/Actions/AL-Go-Helper.ps1' -folder $tmpFolder
+DownloadHelperFile -url 'https://raw.githubusercontent.com/aholstrup1/AL-Go/private/aholstrup/555754_refactor/Actions/Packages.json' -folder $tmpFolder | Out-Null
 
 Import-Module $GitHubHelperPath
 . $ALGoHelperPath -local
