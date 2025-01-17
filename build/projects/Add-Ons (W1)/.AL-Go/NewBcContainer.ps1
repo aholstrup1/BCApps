789e6c4ea5f73e02930e219fec655d4db274fbb9
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2025-01-17 09:06:32 +0000

    Test

--- build/projects/Add-Ons (W1)/.AL-Go/NewBcContainer.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/NewBcContainer.ps1
@@ -1,6 +1,12 @@
 Param(
     [Hashtable]$parameters
 )
+$keepApps = @()
+
+$projectSettings = Get-Content (Join-Path $PSScriptRoot "settings.json" -Resolve) | ConvertFrom-Json
+if ($projectSettings.useProjectDependencies -eq $false) {
+    $keepApps = @("System Application", "Business Foundation", "Base Application", "Application")
+}
 
 $script = Join-Path $PSScriptRoot "../../../scripts/NewBcContainer.ps1" -Resolve
-. $script -parameters $parameters -keepApps @("System Application", "Business Foundation", "Base Application", "Application")
+. $script -parameters $parameters -keepApps $keepApps
