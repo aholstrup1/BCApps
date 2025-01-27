bff2a34e358827820a9986f938b8a235da4b68eb
aholstrup <aholstrup@microsoft.com>
2025-01-27 11:07:11 +0100

    execute permissions

--- build/projects/Add-Ons (W1)/.AL-Go/PreCompileApp.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/PreCompileApp.ps1
@@ -7,6 +7,5 @@ if($appType -eq 'app')
 {
     Write-Host "compilationParams: $compilationParams"
 
-    $scriptPath = Join-Path $PSScriptRoot "../../../scripts/VerifyExecutePermissions.ps1" -Resolve
     . $scriptPath -AppFolder $compilationParams.Value["appProjectFolder"]
 }
