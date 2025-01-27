e30a9265c7ced6a313612c5e94b4ded5251fcbbf
aholstrup <aholstrup@microsoft.com>
2025-01-27 10:46:55 +0100

    Test

---
+++ build/scripts/AppExtensionsHelper.psm1
@@ -0,0 +1,8 @@
+function BuildAppFromBCArtifacts() {
+    param(
+        $bla = "bla"
+    )
+
+    $applicationsPath = "C:\bcartifacts.cache\sandbox\26.0.28812.0\platform\applications\" #TODO: Get this somehow
+    
+}
