ba1c31d44a08bc9f20fc98c16668d24b986d4bbc
aholstrup <aholstrup@microsoft.com>
2025-01-28 15:43:38 +0100

    Filter

--- build/scripts/PreCompileApp.ps1
+++ build/scripts/PreCompileApp.ps1
@@ -53,7 +53,7 @@ if($appType -eq 'app')
                     # Copy apps to packagecachepath
                     $projectFolder = $parameters.Value["appProjectFolder"]
                     # Look for all .app files in project folder
-                    $appFiles = Get-ChildItem -Path $projectFolder -Filter "*Base Application.app"
+                    $appFiles = Get-ChildItem -Path $projectFolder -Filter "*.app"
                     Write-Host "Copying apps to packagecachepath"
                     Write-Host "Found $($appFiles.Count) apps"
                     foreach ($appFile in $appFiles) {
