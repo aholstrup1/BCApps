691e529ce13256a1996d7b18e49372ccb3f7a761
aholstrup <aholstrup@microsoft.com>
2025-02-05 14:09:30 +0100

    Logging

--- build/projects/Add-Ons (W1)/.AL-Go/ImportTestToolkitToBcContainer.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/ImportTestToolkitToBcContainer.ps1
@@ -7,3 +7,8 @@ Import-TestToolkitToBcContainer @parameters
 
 Import-Module (Join-Path $PSScriptRoot "../../../scripts/DevEnv/NewDevContainer.psm1" -Resolve)
 Setup-ContainerForDevelopment -ContainerName $parameters.ContainerName -RepoVersion "26.0"
+
+$installedApps = Get-BcContainerAppInfo -containerName $parameters.ContainerName -tenantSpecificProperties -sort DependenciesLast
+$installedApps | ForEach-Object {
+    Write-Host "$($_.Name) - $($_.Version) - $($_.Tenant) - $($_.IsPublished) / $($_.IsInstalled)"
+}
