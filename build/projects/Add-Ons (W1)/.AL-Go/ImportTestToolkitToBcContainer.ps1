cbaa71436ca129f649cda8b0e0e53b7ce9ec6074
aholstrup <aholstrup@microsoft.com>
2025-02-17 15:51:09 +0100

    Logging

---
+++ build/projects/Add-Ons (W1)/.AL-Go/ImportTestToolkitToBcContainer.ps1
@@ -0,0 +1,12 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'parameters', Justification = 'The parameter is not used, but it''s script needs to match this format')]
+Param(
+    [hashtable] $parameters
+)
+
+# Print all installed apps 
+$installedApps = Get-BcContainerAppInfo -containerName $parameters.containerName -tenantSpecificProperties -sort DependenciesLast
+foreach($app in $installedApps) {
+    Write-Host "$($app.Name) - $($app.Version) - $($app.Scope) - $($app.Tenant) - $($app.IsPublished) - $($app.IsInstalled)"
+}
+
+Import-TestToolkitToBcContainer @parameters
