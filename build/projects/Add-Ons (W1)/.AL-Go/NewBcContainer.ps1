b64cea93811901ac0d5fd04f453a4852e4735c99
aholstrup <aholstrup@microsoft.com>
2025-02-18 15:20:49 +0100

    Try keeping apps

--- build/projects/Add-Ons (W1)/.AL-Go/NewBcContainer.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/NewBcContainer.ps1
@@ -2,19 +2,17 @@ Param(
     [Hashtable]$parameters
 )
 
-function Get-AppsInFolder() {
-    param(
-        [string]$folder
-    )
-    # Look for all app.json files in the folder or subfolders. Grab the "Name" property from the file
-    $apps = Get-ChildItem -Path $folder -Recurse -Filter "app.json" | ForEach-Object {
-        $appJson = Get-Content -Path $_.FullName | ConvertFrom-Json
-        $appJson.Name
-    }
+$keepApps = @(
+    "System Application",
+    "Business Foundation", 
+    "Base Application", 
+    "Application",
+    "Any",
+    "Library Assert",
+    "Library Variable Storage",
+    "System Application Test Library",
+    "Tests-TestLibraries"
+)
 
-    return @($apps)
-}
-
-$apps = Get-AppsInFolder -folder (Join-Path $PSScriptRoot "../../../../src/Add-Ons/W1" -Resolve)
 $script = Join-Path $PSScriptRoot "../../../scripts/NewBcContainer.ps1" -Resolve
-. $script -parameters $parameters -uninstallApps @("None") -moveAppsToDevScope $apps
+. $script -parameters $parameters -keepApps $keepApps
--- build/scripts/NewBcContainer.ps1
+++ build/scripts/NewBcContainer.ps1
@@ -1,7 +1,6 @@
 Param(
     [Hashtable]$parameters,
-    [string[]]$uninstallApps = @("*"),
-    [string[]]$moveAppsToDevScope
+    [string[]]$keepApps = @()
 )
 
 $parameters.multitenant = $false
@@ -16,22 +15,10 @@ New-BcContainer @parameters
 
 $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
 foreach($app in $installedApps) {
-    if (($uninstallApps -contains $app.Name) -or ($uninstallApps -contains "*")) {
-        Write-Host "Removing $($app.Name)"
-        Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $app.Name -unInstall -doNotSaveData -doNotSaveSchema -force
+    if (($keepApps -notcontains $app.Name)) {
+        Write-Host "Uninstalling $($app.Name)"
+        UnInstall-BcContainerApp -containerName $parameters.ContainerName -name $app.Name -doNotSaveData -doNotSaveSchema -force
     }
-}
-
-# Print all installed apps 
-$installedApps = Get-BcContainerAppInfo -containerName $parameters.containerName -tenantSpecificProperties -sort DependenciesLast
-foreach($app in $installedApps) {
-    Write-Host "$($app.Name) - $($app.Version) - $($app.Scope) - $($app.Tenant) - $($app.IsPublished) - $($app.IsInstalled)"
-}
-
-Import-Module (Join-Path $PSScriptRoot "DevEnv/NewDevContainer.psm1" -Resolve) -Force
-
-if ($moveAppsToDevScope) {
-    Setup-ContainerForDevelopment -containerName $parameters.ContainerName -RepoVersion "26.0" -SelectedApps $moveAppsToDevScope
 }
 
 Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
