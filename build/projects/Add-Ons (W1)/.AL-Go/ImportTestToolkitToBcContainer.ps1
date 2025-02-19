8ab229e2b034aa8b182553cf028cd390850749bc
aholstrup <aholstrup@microsoft.com>
2025-02-19 11:51:38 +0100

    Skip entirely

--- build/projects/Add-Ons (W1)/.AL-Go/ImportTestToolkitToBcContainer.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/ImportTestToolkitToBcContainer.ps1
@@ -10,15 +10,3 @@ foreach($app in $installedApps) {
 }
 
 #Import-TestToolkitToBcContainer @parameters
-
-# Ordered list of test framework apps to install
-$allApps = (Invoke-ScriptInBCContainer -containerName $containerName -scriptblock { Get-ChildItem -Path "C:\Applications\" -Filter "*.app" -Recurse })
-$testToolkitApps = @(
-    "Tests-TestLibraries"
-)
-
-foreach ($app in $testToolkitApps) {
-    $appFile = $allApps | Where-Object { $($_.Name) -eq "Microsoft_$($app).app" }
-    Publish-BcContainerApp -containerName $containerName -appFile ":$($appFile.FullName)" -skipVerification -scope Global -install -sync
-    $appFile = $null
-} 
