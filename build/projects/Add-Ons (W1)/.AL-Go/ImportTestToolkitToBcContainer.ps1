963e2344cf8e0b6430af4324eec13ab0b10f6dbd
aholstrup <aholstrup@microsoft.com>
2025-02-19 11:02:37 +0100

    Manually install dependency

--- build/projects/Add-Ons (W1)/.AL-Go/ImportTestToolkitToBcContainer.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/ImportTestToolkitToBcContainer.ps1
@@ -9,4 +9,16 @@ foreach($app in $installedApps) {
     Write-Host "$($app.Name) - $($app.Version) - $($app.Scope) - $($app.Tenant) - $($app.IsPublished) - $($app.IsInstalled)"
 }
 
-Import-TestToolkitToBcContainer @parameters
+#Import-TestToolkitToBcContainer @parameters
+
+# Ordered list of test framework apps to install
+$allApps = (Invoke-ScriptInBCContainer -containerName $containerName -scriptblock { Get-ChildItem -Path "C:\Applications\" -Filter "*.app" -Recurse })
+$testToolkitApps = @(
+    "Tests-TestLibraries"
+)
+
+foreach ($app in $testToolkitApps) {
+    $appFile = $allApps | Where-Object { $($_.Name) -eq "Microsoft_$($app).app" }
+    Publish-BcContainerApp -containerName $containerName -appFile ":$($appFile.FullName)" -skipVerification -scope Global -install -sync
+    $appFile = $null
+} 
