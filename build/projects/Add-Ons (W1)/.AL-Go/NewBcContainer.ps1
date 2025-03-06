98a1ab8531570435c24d5897cad7b508fb58e980
aholstrup <aholstrup@microsoft.com>
2025-03-06 12:15:41 +0100

    Test useProjectDependencies false

--- build/projects/Add-Ons (W1)/.AL-Go/NewBcContainer.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/NewBcContainer.ps1
@@ -2,17 +2,22 @@ Param(
     [Hashtable]$parameters
 )
 
-$keepApps = @(
-    "System Application",
-    "Business Foundation", 
-    "Base Application", 
-    "Application",
-    "Any",
-    "Library Assert",
-    "Library Variable Storage",
-    "System Application Test Library",
-    "Tests-TestLibraries"
-)
+$projectSettings = Get-Content "$PSScriptRoot/settings.json" | ConvertFrom-Json
+
+$keepApps = @()
+if ($projectSettings.useProjectDependencies -eq $false) {
+    $keepApps = @(
+        "System Application",
+        "Business Foundation", 
+        "Base Application", 
+        "Application",
+        "Any",
+        "Library Assert",
+        "Library Variable Storage",
+        "System Application Test Library",
+        "Tests-TestLibraries"
+    )
+}
 
 $script = Join-Path $PSScriptRoot "../../../scripts/NewBcContainer.ps1" -Resolve
-. $script -parameters $parameters -keepApps @() # $keepApps
+. $script -parameters $parameters -keepApps $keepApps
