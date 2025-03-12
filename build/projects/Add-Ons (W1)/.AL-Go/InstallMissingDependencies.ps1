641f81a59abf5b53002f756444c58132f4541d00
aholstrup <aholstrup@microsoft.com>
2025-03-12 16:03:13 +0100

    Reinstall apps before running tests

--- build/projects/Add-Ons (W1)/.AL-Go/InstallMissingDependencies.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/InstallMissingDependencies.ps1
@@ -2,19 +2,6 @@ Param(
     [hashtable] $parameters
 )
 
-# Reinstall all the apps we uninstalled
-$allAppsInEnvironment = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesFirst
-foreach ($app in $allAppsInEnvironment) {
-    $isAppAlreadyInstalled = $allAppsInEnvironment | Where-Object { ($($_.Name) -eq $app.Name) -and ($_.IsInstalled -eq $true) }
-    if (($app.IsInstalled -eq $true) -or ($isAppAlreadyInstalled)) {
-        Write-Host "$($app.Name) is already installed"
-    } else {
-        Write-Host "Installing $($app.Name)"
-        Sync-BcContainerApp -containerName $containerName -appName $app.Name -appPublisher $app.Publisher -Mode ForceSync -Force
-        Install-BcContainerApp -containerName $containerName -appName $app.Name -appPublisher $app.Publisher -appVersion $app.Version -Force
-    }
-}
-
 $allApps = (Invoke-ScriptInBCContainer -containerName $containerName -scriptblock { Get-ChildItem -Path "C:\Applications\" -Filter "*.app" -Recurse })
 foreach ($dependency in $parameters["missingDependencies"]) {
     # Format the dependency variable is AppId:Filename
