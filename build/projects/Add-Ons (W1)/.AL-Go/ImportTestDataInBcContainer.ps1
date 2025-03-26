adf3e71bac44622e9e8e031547e9e841d95d0ac6
aholstrup <aholstrup@microsoft.com>
2025-03-26 12:00:59 +0100

    Regenerate demodata

---
+++ build/projects/Add-Ons (W1)/.AL-Go/ImportTestDataInBcContainer.ps1
@@ -0,0 +1,40 @@
+Param(
+    [Parameter(ParameterSetName="ALGo")]
+    [Hashtable]$parameters,
+    [Parameter(ParameterSetName="Manual")]
+    [string]$containerName
+)
+
+function Get-NavDefaultCompanyName
+{
+    return "CRONUS International Ltd."
+}
+
+if ($PSCmdlet.ParameterSetName -eq 'ALGo') {
+    $containerName = $parameters.ContainerName
+}
+
+$projectSettings = Get-Content "$PSScriptRoot/settings.json" | ConvertFrom-Json
+
+if ($projectSettings.useProjectDependencies -eq $true) {
+    # Get the repoversion
+    Import-Module "$PSScriptRoot\EnlistmentHelperFunctions.psm1"
+    $repoVersion = Get-ConfigValue -ConfigType "AL-GO" -Key "RepoVersion"
+    $DemoDataType = "EXTENDED"
+
+    Write-Host "Initializing company"
+    Invoke-NavContainerCodeunit -Codeunitid 2 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName)
+
+    Write-Host "Importing configuration package"
+    Invoke-NavContainerCodeunit -Codeunitid 8620 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName) -MethodName "ImportAndApplyRapidStartPackage" -Argument "C:\ConfigurationPackages\NAV$($repoVersion).W1.ENU.$($DemoDataType).rapidstart"
+
+    <#
+    Write-Host "Initializing company"
+    Invoke-NavContainerCodeunit -Codeunitid 2 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName)
+
+    Write-Host "Importing configuration package"
+    Invoke-NavContainerCodeunit -CodeunitId 5193 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName) -MethodName "CreateAllDemoData"
+    Invoke-NavContainerCodeunit -CodeunitId 5140 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName) -MethodName "DeleteWarehouseEmployee"
+#>
+}
+
