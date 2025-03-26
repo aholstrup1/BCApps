48fd043ca791e7aee3bfc1b12a0e528cf6d98d47
aholstrup <aholstrup@microsoft.com>
2025-03-26 16:56:30 +0100

    Use contoso dataset

--- build/projects/Add-Ons (W1)/.AL-Go/ImportTestDataInBcContainer.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/ImportTestDataInBcContainer.ps1
@@ -19,22 +19,19 @@ $projectSettings = Get-Content "$PSScriptRoot/settings.json" | ConvertFrom-Json
 if ($projectSettings.useProjectDependencies -eq $true) {
     # Get the repoversion
     #Import-Module "$PSScriptRoot\EnlistmentHelperFunctions.psm1"
-    $repoVersion = "27.0" #Get-ConfigValue -ConfigType "AL-GO" -Key "RepoVersion"
-    $DemoDataType = "EXTENDED"
-
-    Write-Host "Initializing company"
-    Invoke-NavContainerCodeunit -Codeunitid 2 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName)
-
-    Write-Host "Importing configuration package"
-    Invoke-NavContainerCodeunit -Codeunitid 8620 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName) -MethodName "ImportAndApplyRapidStartPackage" -Argument "C:\ConfigurationPackages\NAV$($repoVersion).W1.ENU.$($DemoDataType).rapidstart"
-
-    <#
-    Write-Host "Initializing company"
+    
+    Write-Host "Initializing company in container $containerName"
     Invoke-NavContainerCodeunit -Codeunitid 2 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName)
 
     Write-Host "Importing configuration package"
     Invoke-NavContainerCodeunit -CodeunitId 5193 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName) -MethodName "CreateAllDemoData"
     Invoke-NavContainerCodeunit -CodeunitId 5140 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName) -MethodName "DeleteWarehouseEmployee"
-#>
+
+
+    <#Write-Host "Initializing company"
+    Invoke-NavContainerCodeunit -Codeunitid 2 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName)
+
+    Write-Host "Importing configuration package"
+    Invoke-NavContainerCodeunit -Codeunitid 8620 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName) -MethodName "ImportAndApplyRapidStartPackage" -Argument "C:\ConfigurationPackages\NAV$($repoVersion).W1.ENU.$($DemoDataType).rapidstart"#>
 }
 
