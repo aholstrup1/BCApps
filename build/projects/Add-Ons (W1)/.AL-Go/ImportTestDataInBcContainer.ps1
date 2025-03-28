4589d64b6676154e6706ae7275924d04f5b64a2b
aholstrup <aholstrup@microsoft.com>
2025-03-28 14:49:00 +0100

    Refactor

--- build/projects/Add-Ons (W1)/.AL-Go/ImportTestDataInBcContainer.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/ImportTestDataInBcContainer.ps1
@@ -4,11 +4,3 @@ Param(
 
 $script = Join-Path $PSScriptRoot "../../../scripts/ImportTestDataInBcContainer.ps1" -Resolve
 . $script -parameters $parameters
-
-<#$projectSettings = Get-Content "$PSScriptRoot/settings.json" | ConvertFrom-Json
-if ($projectSettings.useProjectDependencies -eq $true) {
-    
-} else {
-    # TODO: This should be fixed so we always import demo data
-    Write-Host "Demo data is already imported, skipping importing demo data"
-}#>
--- build/scripts/NewBcContainer.ps1
+++ build/scripts/NewBcContainer.ps1
@@ -1,7 +1,6 @@
 Param(
     [Hashtable]$parameters,
-    [string[]]$keepApps = @(),
-    [boolean]$useProjectDependencies = $true
+    [string[]]$keepApps = @()
 )
 
 $parameters.multitenant = $false
@@ -36,22 +35,6 @@ PrepareEnvironment -ContainerName $parameters.ContainerName -KeepApps ($keepApps
 
 foreach ($app in (Get-BcContainerAppInfo -containerName $parameters.ContainerName -tenantSpecificProperties -sort DependenciesLast)) {
     Write-Host "App: $($app.Name) ($($app.Version)) - Scope: $($app.Scope) - $($app.IsInstalled) / $($app.IsPublished)"
-}
-
-# Reinstall all the apps we uninstalled
-$allAppsInEnvironment = Get-BcContainerAppInfo -containerName $parameters.ContainerName -tenantSpecificProperties -sort DependenciesFirst
-foreach ($app in $allAppsInEnvironment) {
-    $isAppAlreadyInstalled = $allAppsInEnvironment | Where-Object { ($($_.Name) -eq $app.Name) -and ($_.IsInstalled -eq $true) }
-    $shouldInstall = $KeepApps | Where-Object { $_ -eq $app.Name }
-    if (($app.IsInstalled -eq $true) -or ($isAppAlreadyInstalled)) {
-        Write-Host "$($app.Name) is already installed"
-    } elseif(-not $shouldInstall) {
-        Write-Host "$($app.Name) is not in the list of dependencies to install. Leaving it uninstalled."
-    } else {
-        Write-Host "Installing $($app.Name)"
-        Sync-BcContainerApp -containerName $parameters.ContainerName -appName $app.Name -appPublisher $app.Publisher -Mode ForceSync -Force
-        Install-BcContainerApp -containerName $parameters.ContainerName -appName $app.Name -appPublisher $app.Publisher -appVersion $app.Version -Force
-    }
 }
 
 Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
