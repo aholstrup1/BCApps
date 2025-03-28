5878a9e363d676fa609e47d20317f91307262e2a
aholstrup <aholstrup@microsoft.com>
2025-03-28 13:42:19 +0100

    refactor

--- build/scripts/NewBcContainer.ps1
+++ build/scripts/NewBcContainer.ps1
@@ -17,46 +17,41 @@ New-BcContainer @parameters
 function PrepareEnvironment() {
     param(
         [string] $ContainerName,
-        [boolean] $UseProjectDependencies,
-        [string[]] $KeepApps = @()
+        [boolean] $KeepApps = $false
     )
     $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
 
-    if ($UseProjectDependencies) {
-        # Clean the container for all apps. Apps will be installed by AL-Go
-        foreach($app in $installedApps) {
-            UnInstall-BcContainerApp -containerName $parameters.ContainerName -name $app.Name -doNotSaveData -doNotSaveSchema -force
+    # Clean the container for all apps. Apps will be installed by AL-Go
+    foreach($app in $installedApps) {
+        UnInstall-BcContainerApp -containerName $ContainerName -name $app.Name -doNotSaveData -doNotSaveSchema -force
 
-            if ((-not $KeepApps)) {
-                Write-Host "Unpublishing $($app.Name)"
-                Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $app.Name -unInstall -doNotSaveData -doNotSaveSchema -force
-            } else {
-                Write-Host "Uninstalling $($app.Name)"
-                UnInstall-BcContainerApp -containerName $parameters.ContainerName -name $app.Name -doNotSaveData -doNotSaveSchema -force
-            }
-        }  
-    } else {
-        # Keep the apps that are in the keepApps list
-        # Unpublish apps that are not installed 
-        # Uninstall apps that are installed
-        foreach($app in $installedApps) {
-            if (($keepApps -notcontains $app.Name)) {
-                if ($app.IsInstalled -eq $false) {
-                    Write-Host "Unpublishing $($app.Name)"
-                    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $app.Name -unInstall -doNotSaveData -doNotSaveSchema -force
-                } else {
-                    Write-Host "Uninstalling $($app.Name)"
-                    UnInstall-BcContainerApp -containerName $parameters.ContainerName -name $app.Name -doNotSaveData -doNotSaveSchema -force
-                }
-            }
+        if ((-not $KeepApps)) {
+            Write-Host "Unpublishing $($app.Name)"
+            Unpublish-BcContainerApp -containerName $ContainerName -name $app.Name -unInstall -doNotSaveData -doNotSaveSchema -force
         }
     }
 }
 
-PrepareEnvironment -ContainerName $parameters.ContainerName -UseProjectDependencies $useProjectDependencies -KeepApps $keepApps
+PrepareEnvironment -ContainerName $parameters.ContainerName -KeepApps ($keepApps.Count -gt 0)
 
 foreach ($app in (Get-BcContainerAppInfo -containerName $parameters.ContainerName -tenantSpecificProperties -sort DependenciesLast)) {
     Write-Host "App: $($app.Name) ($($app.Version)) - Scope: $($app.Scope) - $($app.IsInstalled) / $($app.IsPublished)"
+}
+
+# Reinstall all the apps we uninstalled
+$allAppsInEnvironment = Get-BcContainerAppInfo -containerName $parameters.ContainerName -tenantSpecificProperties -sort DependenciesFirst
+foreach ($app in $allAppsInEnvironment) {
+    $isAppAlreadyInstalled = $allAppsInEnvironment | Where-Object { ($($_.Name) -eq $app.Name) -and ($_.IsInstalled -eq $true) }
+    $shouldInstall = $KeepApps | Where-Object { $_ -eq $app.Name }
+    if (($app.IsInstalled -eq $true) -or ($isAppAlreadyInstalled)) {
+        Write-Host "$($app.Name) is already installed"
+    } elseif(-not $shouldInstall) {
+        Write-Host "$($app.Name) is not in the list of dependencies to install. Leaving it uninstalled."
+    } else {
+        Write-Host "Installing $($app.Name)"
+        Sync-BcContainerApp -containerName $parameters.ContainerName -appName $app.Name -appPublisher $app.Publisher -Mode ForceSync -Force
+        Install-BcContainerApp -containerName $parameters.ContainerName -appName $app.Name -appPublisher $app.Publisher -appVersion $app.Version -Force
+    }
 }
 
 Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
