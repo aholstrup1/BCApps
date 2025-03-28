1bb5a725536eca7ff211d5c0ea7ee9f77c0257c2
aholstrup <aholstrup@microsoft.com>
2025-03-28 08:39:19 +0100

    Skip importing demo data when useprojectdependencies false

--- build/projects/Add-Ons (W1)/.AL-Go/ImportTestDataInBcContainer.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/ImportTestDataInBcContainer.ps1
@@ -2,5 +2,11 @@ Param(
     [Hashtable]$parameters
 )
 
-$script = Join-Path $PSScriptRoot "../../../scripts/ImportTestDataInBcContainer.ps1" -Resolve
-. $script -parameters $parameters
+$projectSettings = Get-Content "$PSScriptRoot/settings.json" | ConvertFrom-Json
+if ($projectSettings.useProjectDependencies -eq $true) {
+    $script = Join-Path $PSScriptRoot "../../../scripts/ImportTestDataInBcContainer.ps1" -Resolve
+    . $script -parameters $parameters
+} else {
+    # TODO: This should be fixed so we always import demo data
+    Write-Host "Demo data is already imported, skipping importing demo data"
+}
