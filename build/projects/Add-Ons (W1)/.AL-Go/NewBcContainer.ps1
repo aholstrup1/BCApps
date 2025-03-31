787cc4c0bd3c882e380acdfa69b8f552c85ecf39
aholstrup <aholstrup@microsoft.com>
2025-03-31 10:59:30 +0200

    Change parameter on newbccontainer

--- build/projects/Add-Ons (W1)/.AL-Go/NewBcContainer.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/NewBcContainer.ps1
@@ -2,9 +2,5 @@ Param(
     [Hashtable]$parameters
 )
 
-# Determine the external dependencies
-$customSettings = Get-Content -Path (Join-Path $PSScriptRoot "customSettings.json" -Resolve) | ConvertFrom-Json
-$externalDependencies = $customSettings.ExternalAppDependencies
-
 $script = Join-Path $PSScriptRoot "../../../scripts/NewBcContainer.ps1" -Resolve
-. $script -parameters $parameters -keepApps $externalDependencies
+. $script -parameters $parameters -KeepAppsPublished
