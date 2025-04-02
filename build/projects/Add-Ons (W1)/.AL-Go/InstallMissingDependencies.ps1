4a8d175d2de5857e21e6c8db445642460294e9e6
aholstrup <aholstrup@microsoft.com>
2025-04-02 07:52:31 +0200

    Refactor installmissing dependencies

--- build/projects/Add-Ons (W1)/.AL-Go/InstallMissingDependencies.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/InstallMissingDependencies.ps1
@@ -2,13 +2,17 @@ Param(
     [hashtable] $parameters
 )
 
-function Publish-AppsFromFile() {
+function Publish-AppFromFile() {
     param(
+        [Parameter(Mandatory = $true)]
         [string] $ContainerName,
+        [Parameter(Mandatory = $true, ParameterSetName = "ByAppFilePath")]
         [string] $AppFilePath,
+        [Parameter(Mandatory = $true, ParameterSetName = "ByAppName")]
         [string] $AppName
     )
-    if (-not $AppFilePath) {
+    if ($PSCmdlet.ParameterSetName -eq "ByAppName") {
+        Write-Host "Searching for app file with name: $AppName"
         $allApps = (Invoke-ScriptInBCContainer -containerName $ContainerName -scriptblock { Get-ChildItem -Path "C:\Applications\" -Filter "*.app" -Recurse })
         $AppFilePath = $allApps | Where-Object { $($_.BaseName) -like "*$($AppName)" } | ForEach-Object { $_.FullName }
     }
@@ -21,7 +25,7 @@ function Publish-AppsFromFile() {
     Publish-BcContainerApp -containerName $ContainerName -appFile ":$($AppFilePath)" -skipVerification -scope Global -install -sync
 }
 
-function InstallDependencies() {
+function Install-MissingDependencies() {
     param(
         [Parameter(Mandatory = $true)]
         [string] $ContainerName,
@@ -33,20 +37,20 @@ function InstallDependencies() {
     foreach($dependency in $DependenciesToInstall) {
         $appInContainer = $allAppsInEnvironment | Where-Object Name -eq $dependency
         if (-not $appInContainer) {
-            Write-Host "$($dependency) is not published to the container"
+            Write-Host "[Install Missing Dependencies] - $($dependency) is not published to the container"
             $missingDependencies += $dependency
             continue
         }
 
         $isAppInstalled = $appInContainer | Where-Object IsInstalled -eq $true
         if ($isAppInstalled) {
-            Write-Host "$($dependency) ($($isAppInstalled.Version)) is already installed"
+            Write-Host "[Install Missing Dependencies] - $($dependency) ($($isAppInstalled.Version)) is already installed"
             continue
         }
         
         $uninstalledApps = @($appInContainer | Where-Object IsInstalled -eq $false)
         if ($uninstalledApps.Count -gt 1) {
-            throw "$($dependency) has multiple versions published. Cannot determine which one to install"
+            throw "[Install Missing Dependencies] - $($dependency) has multiple versions published. Cannot determine which one to install"
         }
 
         $appToInstall = $uninstalledApps[0]
@@ -54,35 +58,21 @@ function InstallDependencies() {
         Sync-BcContainerApp -containerName $ContainerName -appName $appToInstall.Name -appPublisher $appToInstall.Publisher -Mode ForceSync -Force
         Install-BcContainerApp -containerName $ContainerName -appName $appToInstall.Name -appPublisher $appToInstall.Publisher -appVersion $appToInstall.Version -Force
     }
+
+    if ($missingDependencies.Count -gt 0) {
+        Write-Host "[Install Missing Dependencies] - The following dependencies are missing: $($missingDependencies -join ', ')"
+    }
+    return $missingDependencies
 }
+
 # Reinstall the dependencies in the container
 $customSettings = Get-Content -Path (Join-Path $PSScriptRoot "customSettings.json" -Resolve) | ConvertFrom-Json
 $dependenciesToInstall = $customSettings.ExternalAppDependencies
-InstallDependencies -ContainerName $containerName -DependenciesToInstall $dependenciesToInstall
 
-# When we use project dependencies we get the test toolkit from 
-$projectSettings = Get-Content "$PSScriptRoot/settings.json" | ConvertFrom-Json
-if ($projectSettings.useProjectDependencies -eq $false) {
-    Import-TestToolkitToBcContainer -containerName $containerName
-} else {
-    # Install Tests-TestLibraries - Remaining test libraries should be in BCApps so they are installed as project dependencies
-    Publish-AppsFromFile -ContainerName $containerName -AppName "Tests-TestLibraries"
-}
+# Step 1: If the app is published to the container then we can install it from there
+$dependenciesToInstall = Install-MissingDependencies -ContainerName $containerName -DependenciesToInstall $dependenciesToInstall
 
-<#
-# Reinstall the dependencies in the container
-$allAppsInEnvironment = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesFirst
-foreach ($app in $allAppsInEnvironment) {
-    $isAppAlreadyInstalled = $allAppsInEnvironment | Where-Object { ($($_.Name) -eq $app.Name) -and ($_.IsInstalled -eq $true) }
-    $shouldInstall = $dependenciesToInstall | Where-Object { $_ -eq $app.Name }
-    if (($app.IsInstalled -eq $true) -or ($isAppAlreadyInstalled)) {
-        Write-Host "$($app.Name) is already installed"
-    } elseif(-not $shouldInstall) {
-        Write-Host "$($app.Name) is not in the list of dependencies to install"
-    } else {
-        Write-Host "Installing $($app.Name)"
-        Sync-BcContainerApp -containerName $containerName -appName $app.Name -appPublisher $app.Publisher -Mode ForceSync -Force
-        Install-BcContainerApp -containerName $containerName -appName $app.Name -appPublisher $app.Publisher -appVersion $app.Version -Force
-    }
+# Step 2: If the app is not published to the container then we need to install it from the file system
+foreach ($dependency in $dependenciesToInstall) {
+    Publish-AppFromFile -ContainerName $containerName -AppName $dependency
 }
-#>
