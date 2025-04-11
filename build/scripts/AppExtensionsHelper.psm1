48a4b07851e38ad83b878af87c13d80441d6a4ca
aholstrup <aholstrup@microsoft.com>
2025-04-11 13:23:26 +0200

    fix

--- build/scripts/AppExtensionsHelper.psm1
+++ build/scripts/AppExtensionsHelper.psm1
@@ -87,7 +87,7 @@ function Build-Dependency() {
     }
 
     # If app is already there then skip it - TODO this shouldn't happen when useprojectfolder is set to false
-    $isAppThere = Get-ChildItem -Path $addOnsSymbolsFolder -Filter -Include *.app | Where-Object { $_.BaseName -like "*Microsoft_$App_*" }
+    $isAppThere = Get-ChildItem -Path $addOnsSymbolsFolder -Include *.app | Where-Object { $_.BaseName -like "*Microsoft_$App_*" }
     if ($isAppThere) {
         Write-Host "$App is already in the symbols folder. Skipping recompilation"
         return
