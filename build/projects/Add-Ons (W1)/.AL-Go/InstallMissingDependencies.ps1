0d8d62c37347c9a2ae35cab365598befd16f049d
aholstrup <aholstrup@microsoft.com>
2025-04-24 15:44:18 +0200

    bad merge

--- build/projects/Add-Ons (W1)/.AL-Go/InstallMissingDependencies.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/InstallMissingDependencies.ps1
@@ -14,15 +14,5 @@ if (-not $isBaseAppInstalled) {
     $dependenciesToInstall = Get-ExternalDependencies -TestAppDependencies
 }
 
-
-$isBaseAppInstalled = Get-BcContainerAppInfo -containerName $parameters.containerName -tenantSpecificProperties | Where-Object { ($_.Name -eq "Base Application") -and ($_.IsInstalled -eq $true) }
-if (-not $isBaseAppInstalled) {
-    Write-Host "Installing App Dependencies"
-    $dependenciesToInstall = $customSettings.ExternalAppDependencies
-} else {
-    Write-Host "Installing Test App Dependencies"
-    $dependenciesToInstall = $customSettings.ExternalTestAppDependencies
-}
-
 $script = Join-Path $PSScriptRoot "../../../scripts/InstallMissingDependencies.ps1" -Resolve
 . $script -parameters $parameters -dependenciesToInstall $dependenciesToInstall
