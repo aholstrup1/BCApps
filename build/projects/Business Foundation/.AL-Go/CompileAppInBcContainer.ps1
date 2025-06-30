0432098cf89406159f12d50c67a00e80cb8eeaaa
aholstrup <aholstrup@microsoft.com>
2025-06-30 10:39:09 +0200

    Recompile

--- build/projects/Business Foundation/.AL-Go/CompileAppInBcContainer.ps1
+++ build/projects/Business Foundation/.AL-Go/CompileAppInBcContainer.ps1
@@ -3,4 +3,4 @@ Param(
 )
 
 $scriptPath = Join-Path $PSScriptRoot "../../../scripts/CompileAppInBcContainer.ps1" -Resolve
-. $scriptPath -parameters $parameters
+. $scriptPath -parameters $parameters -recompileDependencies $("System Application")
